7491330ca3279b07d4e53c27d6debeeb377cda82
AndreyKutasevych <andrdroniuc@gmail.com>
2024-03-29 20:14:58 +0000

    Update jump mechanics

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,54 +1,101 @@
-using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D _body;
     private Animator _animation;
+    private BoxCollider2D _boxCollider;
+    private float _wallJumpCooldown;
+    private float _horizontalInput;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
     [SerializeField] private float speed;
-    private bool _grounded;
-    private static readonly int Grounded = Animator.StringToHash("grounded");
-    private static readonly int Run = Animator.StringToHash("Run");
+    [SerializeField] private float jumpPower;
 
     private void Awake()
     {
         _body = GetComponent<Rigidbody2D>();
         _animation = GetComponent<Animator>();
+        _boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
-    {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        _body.velocity = new Vector2(horizontalInput*speed,_body.velocity.y);
-        if (Input.GetKey(KeyCode.Space)&& _grounded)
-        {
-            Jump();
-        }
+    { 
+        _horizontalInput = Input.GetAxis("Horizontal");
 
-        if (horizontalInput>0.01f)
+        if (_horizontalInput>0.01f)
         {
             transform.localScale = Vector3.one;
         }
-        else if(horizontalInput<-0.01f)
+        else if(_horizontalInput<-0.01f)
         {
             transform.localScale = new Vector3(-1,1,1);
         }
-        _animation.SetBool("Run",horizontalInput!=0);
-        _animation.SetBool("Grounded",_grounded);
+        _animation.SetBool("Run",_horizontalInput!=0);
+        _animation.SetBool("Grounded",IsGrounded());
+        if (_wallJumpCooldown > 0.2f)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Jump();
+            }
+            _body.velocity = new Vector2(_horizontalInput*speed,_body.velocity.y);
+            if (OnWall() && !IsGrounded())
+            {
+                _body.gravityScale = 0;
+                _body.velocity = Vector2.zero;
+            }
+            else
+            {
+                _body.gravityScale = 7f;
+            }
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Jump();
+            }
+        }
+        else
+        {
+            _wallJumpCooldown+=Time.deltaTime;
+        }
     }
 
     private void Jump()
     {
-        _body.velocity = new Vector2(_body.velocity.x,speed);
-        _grounded = false;
-        _animation.SetTrigger("Jump");
+        if (IsGrounded())
+        {
+            _body.velocity = new Vector2(_body.velocity.x,jumpPower);
+            _animation.SetTrigger("Jump");
+        }
+        else if(OnWall()&& !IsGrounded())
+        {
+            if (_horizontalInput==0)
+            {
+                _body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x)*10,0);
+                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x),transform.localScale.y,transform.localScale.z);
+            }
+            else
+            {
+                _body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x)*3,6);
+            }
+            _wallJumpCooldown = 0;
+        }
+        
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.tag=="Ground")
-        {
-            _grounded = true;
-        }
+        
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(_boxCollider.bounds.center, _boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+    private bool OnWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(_boxCollider.bounds.center, _boxCollider.bounds.size, 0, new Vector2(transform.localScale.x,0), 0.1f, wallLayer);
+        return raycastHit.collider != null;
     }
 }
