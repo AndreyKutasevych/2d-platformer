8232fd68c4b367a59d6b04fdb0d5b1b1b6f855b7
AndreyKutasevych <andrdroniuc@gmail.com>
2024-03-29 16:10:38 +0000

    Implement basic moving mechanics

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D _body;
+    [SerializeField] private float speed;
+
+    private void Awake()
+    {
+        _body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _body.velocity = new Vector2(Input.GetAxis("Horizontal")*speed,_body.velocity.y);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _body.velocity = new Vector2(_body.velocity.x,speed);
+        }
+    }
+}
