41ad3808a167c1c9ba564694b98449c8d4a2cb6c
AndreyKutasevych <andrdroniuc@gmail.com>
2024-04-01 21:55:56 +0100

    Implement Iframes invulnerability duration

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -1,17 +1,22 @@
-using System;
+using System.Collections;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header("Health")]
     [SerializeField] private float startingHealth;
     private Animator _animator;
     private bool _isDead;
     public float CurrentHealth { get; private set; }
+    [SerializeField] [Header("Iframes")] private float invulnerabilityDuration;
+    [SerializeField] private float flashesAmount;
+    private SpriteRenderer _spriteRenderer;
 
     private void Awake()
     {
         CurrentHealth = startingHealth;
         _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(float damage)
@@ -20,7 +25,8 @@ public class Health : MonoBehaviour
         if (CurrentHealth > 0)
         {
             _animator.SetTrigger("Hurt");
-            
+            StartCoroutine(Invulnerability());
+
         }
         else
         {
@@ -39,4 +45,16 @@ public class Health : MonoBehaviour
         CurrentHealth = Mathf.Clamp(CurrentHealth + hitPoint,0,startingHealth);
     }
 
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(10,11,true);
+        for (int i = 0; i < flashesAmount; i++)
+        {
+            _spriteRenderer.color = new Color(1, 0, 0,0.5f);
+            yield return new WaitForSeconds(invulnerabilityDuration/flashesAmount/2);
+            _spriteRenderer.color = Color.white;
+            yield return new WaitForSeconds(invulnerabilityDuration/flashesAmount/2);
+        }
+        Physics2D.IgnoreLayerCollision(10,11,false);
+    }
 }
